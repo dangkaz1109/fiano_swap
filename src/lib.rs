db0e12baca8a8bb33cd03bfe42f0e5c12cb9f1b4
dangkaz1109 <neurocurior1109@gmail.com>
2025-06-10 17:17:27 +0700

    Hello World!

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+
+fn swap<T: Copy>(a: &mut T, b: &mut T) {
+    let c : T = *a;
+    *a = *b;
+    *b = c;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+    }
+}
